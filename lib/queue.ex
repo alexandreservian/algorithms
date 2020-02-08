aead7a4142f8378750cc4b6d043379bbbf33cca8
alexandreservian <alexandreservian@gmail.com>
2020-02-07 21:20:42 -0300

    criando metodos enqueue, show e size

--- lib/queue.ex
+++ lib/queue.ex
@@ -5,6 +5,25 @@ defmodule Algorithms.Queue do
   end
 
   def loop(stack) do
-
+    receive do
+      {:enqueue, item} ->
+        loop([item | stack])
+      {:show, sender} ->
+        send sender, {:ok, Enum.reverse(stack)}
+      {:size, sender} ->
+        send sender, {:ok, Enum.count(stack)}
+    end
+    loop(stack)
+  end
+  def enqueue(pid, item) do
+    send pid, {:enqueue, item}
+  end
+  def show(pid)do
+    send pid, {:show, self()}
+    receive do {:ok, stack} -> stack end
+  end
+  def size(pid) do
+    send pid, {:size, self()}
+    receive do {:ok, size} -> size end
   end
 end
--- test/queue_test.exs
+++ test/queue_test.exs
@@ -10,13 +10,25 @@ defmodule AlgorithmsTest.Queue do
       assert is_pid(pid)
     end
 
-    test "Algorithms.Queue.enqueue: should be size 4 after 4 call method push" do
+    test "Algorithms.Queue.size: should be 0" do
+      {:ok, pid} = Queue.start_link
+      assert Queue.size(pid) == 0
+    end
+
+    test "Algorithms.Queue.size: should be size 4 after 4 call method size" do
       {:ok, pid} = Queue.start_link
       Queue.enqueue pid, 5
       Queue.enqueue pid, 7
       Queue.enqueue pid, 9
       Queue.enqueue pid, 3
       assert Queue.size(pid) == 4
+    end
+
+    test "Algorithms.Queue.show: should be [15,17]" do
+      {:ok, pid} = Queue.start_link
+      Queue.enqueue pid, 15
+      Queue.enqueue pid, 17
+      assert Queue.show(pid) == [15,17]
     end
   end
 end
