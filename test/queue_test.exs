82b669a0e660256c928f5f024cd9bb7b859f6b30
alexandreservian <alexandreservian@gmail.com>
2020-03-03 11:14:35 -0300

    Testes: adicionel setup para evitar o recuso desnecessario de codigo

--- test/queue_test.exs
+++ test/queue_test.exs
@@ -1,22 +1,23 @@
 defmodule AlgorithmsTest.Queue do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Algorithms
   alias Algorithms.Queue
 
+  setup do
+    {:ok, pid} = Queue.start_link
+    %{pid: pid}
+  end
+
   describe "Algorithms.Queue" do
-    test "Algorithms.Queue.start_link: should be :ok and pid" do
-      {:ok, pid} = Queue.start_link
-      assert :ok == :ok
+    test "Algorithms.Queue.start_link: should be :ok and pid", %{pid: pid} do
       assert is_pid(pid)
     end
 
-    test "Algorithms.Queue.size: should be 0" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.size: should be 0", %{pid: pid}  do
       assert Queue.size(pid) == 0
     end
 
-    test "Algorithms.Queue.size: should be size 4 after 4 call method size" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.size: should be size 4 after 4 call method size", %{pid: pid}  do
       Queue.enqueue pid, 5
       Queue.enqueue pid, 7
       Queue.enqueue pid, 9
@@ -24,42 +25,36 @@ defmodule AlgorithmsTest.Queue do
       assert Queue.size(pid) == 4
     end
 
-    test "Algorithms.Queue.show: should be [15,17]" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.show: should be [15,17]", %{pid: pid}  do
       Queue.enqueue pid, 15
       Queue.enqueue pid, 17
       assert Queue.show(pid) == [15,17]
     end
 
-    test "Algorithms.Queue.dequeue: should remove last element it's 27" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.dequeue: should remove last element it's 27", %{pid: pid}  do
       Queue.enqueue pid, 15
       Queue.enqueue pid, 50
       Queue.enqueue pid, 27
       assert Queue.dequeue(pid) == 15
     end
 
-    test "Algorithms.Queue.peek: should return 50" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.peek: should return 50", %{pid: pid}  do
       Queue.enqueue pid, 15
       Queue.enqueue pid, 78
       Queue.enqueue pid, 50
       assert Queue.peek(pid) == 15
     end
 
-    test "Algorithms.Queue.isEmpty: should return true" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.isEmpty: should return true", %{pid: pid}  do
       assert Queue.isEmpty(pid) == true
     end
 
-    test "Algorithms.Queue.isEmpty: should return false" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.isEmpty: should return false", %{pid: pid}  do
       Queue.enqueue pid, 19
       assert Queue.isEmpty(pid) == false
     end
 
-    test "Algorithms.Queue.clear: should return []" do
-      {:ok, pid} = Queue.start_link
+    test "Algorithms.Queue.clear: should return []", %{pid: pid}  do
       Queue.enqueue pid, 16
       Queue.enqueue pid, 19
       Queue.enqueue pid, 17
--- test/stack_test.exs
+++ test/stack_test.exs
@@ -1,17 +1,19 @@
 defmodule AlgorithmsTest.Stack do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Algorithms
   alias Algorithms.Stack
 
+  setup do
+    {:ok, pid} = Stack.start_link
+    %{pid: pid}
+  end
+
   describe "Algorithms.Stack" do
-    test "Algorithms.Stack.start_link: should be :ok and pid" do
-      {:ok, pid} = Stack.start_link
-      assert :ok == :ok
+    test "Algorithms.Stack.start_link: should be :ok and pid", %{pid: pid} do
       assert is_pid(pid)
     end
 
-    test "Algorithms.Stack.push: should be size 4 after 4 call method push" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.push: should be size 4 after 4 call method push", %{pid: pid} do
       Stack.push pid, 5
       Stack.push pid, 7
       Stack.push pid, 9
@@ -19,42 +21,36 @@ defmodule AlgorithmsTest.Stack do
       assert Stack.size(pid) == 4
     end
 
-    test "Algorithms.Stack.show: should be [15,17]" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.show: should be [15,17]", %{pid: pid} do
       Stack.push pid, 15
       Stack.push pid, 17
       assert Stack.show(pid) == [15,17]
     end
 
-    test "Algorithms.Stack.pop: should remove last element it's 27" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.pop: should remove last element it's 27", %{pid: pid} do
       Stack.push pid, 15
       Stack.push pid, 50
       Stack.push pid, 27
       assert Stack.pop(pid) == 27
     end
 
-    test "Algorithms.Stack.peek: should return 50" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.peek: should return 50", %{pid: pid} do
       Stack.push pid, 15
       Stack.push pid, 78
       Stack.push pid, 50
       assert Stack.peek(pid) == 50
     end
 
-    test "Algorithms.Stack.isEmpty: should return true" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.isEmpty: should return true", %{pid: pid} do
       assert Stack.isEmpty(pid) == true
     end
 
-    test "Algorithms.Stack.isEmpty: should return false" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.isEmpty: should return false", %{pid: pid} do
       Stack.push pid, 19
       assert Stack.isEmpty(pid) == false
     end
 
-    test "Algorithms.Stack.clear: should return []" do
-      {:ok, pid} = Stack.start_link
+    test "Algorithms.Stack.clear: should return []", %{pid: pid} do
       Stack.push pid, 16
       Stack.push pid, 19
       Stack.push pid, 17
